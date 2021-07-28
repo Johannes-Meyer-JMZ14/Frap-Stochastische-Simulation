07418941ee80c0e36c941411f4886191d3da0752
JMZ14 <johannes->
2021-07-28 23:22:06 +0200

    Zielfunktion schreiben

--- Fitting.py
+++ Fitting.py
@@ -54,8 +54,7 @@ def assign_simulation_times_to_time_ranges_average(time_intervals, times, list_g
             # add all matched times to the current gillespie objects list
             averaged_data_for_each_run[i].append(np.mean(data_in_time_range))
 
-    numpy_array = np.array(averaged_data_for_each_run)
-    transposed_array = np.transpose(numpy_array)
+    transposed_array = np.transpose(np.array(averaged_data_for_each_run))
     all_time_averages = pd.DataFrame(transposed_array, index=times)
     return all_time_averages
 # TODO: Mittelwert der Simulationsdaten im Messbereich bestimmen
@@ -72,16 +71,14 @@ def arithmetic_means(dataframe):
     
     return ret
 # TODO: Tests schreiben
-def evaluation(df_measured, df_simulated_means):
+def calculate_differences(series_measured, df_simulated_normalized_means):
     """Evaluates how much a simulations differs from measured data."""
-    # iterated_sum = 0
-    absolutes_sum = 0
-    for col, ind in (df_measured.columns, df_measured.index):
-        difference = df_measured[col][ind]-df_simulated_means[col][ind]
-        # iterated_sum += difference
-        absolutes_sum += abs(difference)
-    
-    return absolutes_sum  # (iterated_sum, absolutes_sum)
+
+    differences = pd.DataFrame(np.zeros(df_simulated_normalized_means.shape), columns=df_simulated_normalized_means.columns, index=df_simulated_normalized_means.index)
+    for run in df_simulated_normalized_means:
+        differences[run] = abs(df_simulated_normalized_means[run]-series_measured)
+
+    return differences
 
 #measurement = pd.read_csv("./../Daten/FRAP_comparison_all_new_AssemblyDynamicsOfPMLNuclearBodiesInLivingCells_cleaned.csv")
 #measured_times = measurement["time[s]"]
